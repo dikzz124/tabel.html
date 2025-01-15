f2f1ec196cc6cd968a0c4fabaa018e394a87f2ff
dikzz124 <rachmandicky17@gmail.com>
2025-01-15 10:59:19 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,17 @@
+
+let nama = "Dicky; 
+let alamat = Semarang city"; 
+let usia = "15";  
+let ttl = "Semarang 4 Mei 2009 "; 
+let gender = "laki laki"; 
+let agama = "islam"; 
+let email = "rachmandicky@gmail.com";
+let berat = "50";
+let tinggi = "170";
+let hobi = "jalan jalan ";
+let cita = "menjadi orang sukses";
+let pekerjaan = "siswa";
+let sekolah = "SMK N 1 SAYUNG";
+let jurusan = "PPLG";
+
+document.write("nama saya adalah " + nama + " saya tinggal di " + alamat + " pada tahun 2024 ini usia saya adalah " + usia + "tahun, saya lahir di " + ttl + "jenis kelamin saya adalah " + gender + "saya beragama " + agama + "alamat email saya adalah " + email + "saya memiliki berat badan " + berat +"kg"+ "dengan tinggi " + tinggi + "saya sangat senang " + hobi + "karena saya memiliki cita cita " + cita + "saya saat adalah seorang " + pekerjaan + "yang bersekolah di " + sekolah + "pada jurusan " + jurusan);
